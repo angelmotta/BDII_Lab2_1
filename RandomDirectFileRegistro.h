44f5ff4f773f1cb17e116859daaac0f59854c299
Angel Motta <angelmotta@gmail.com>
2020-04-17 20:13:19 -0500

    P2 search method implementation done

--- RandomDirectFileRegistro.h
+++ RandomDirectFileRegistro.h
@@ -9,16 +9,147 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 
 class RandomDirectFileRegistro{
 private:
+    std::string indexFile;
     std::string dataFile;
-    std::string auxFile;
-    int rowsDataFile;
-    int rowsAuxFile;
+
+    void updateIndexFile(Registro& registro, int row){
+        std::fstream outIndexFile;
+        outIndexFile.open(indexFile, std::ios::out |std::ios::app | std::ios::binary);
+        if(outIndexFile.is_open()){
+            std::cout << "Write Index\n";
+            outIndexFile.write(registro.getCodigo(),  5);
+            outIndexFile.write((char *)(&row), sizeof(row));
+            outIndexFile << "\n";
+            outIndexFile << std::flush;
+        }
+        outIndexFile.close();
+    }
+
+    void initFiles() {
+        std::cout << "** init Files **\n";
+        std::ofstream outDataFile, outAuxFile;
+        outDataFile.open(dataFile,std::ios::out | std::ios::trunc | std::ios::binary); // trunc 'discard existing content'
+        outDataFile.close();
+        outAuxFile.open(indexFile,std::ios::out | std::ios::trunc | std::ios::binary); // trunc 'discard existing content'
+        outAuxFile.close();
+    }
+
+    void readRecord(int row){
+        std::cout << "- Read record from data File (disk) -\n";
+        Registro record;
+        int sizeRecord = sizeof(record) + 1;
+        std::ifstream inFile;
+        inFile.open(dataFile, std::ios::in | std::ios::binary);
+        inFile.seekg(sizeRecord * (row - 1), std::ios::beg);
+        inFile >> record;
+        record.showMetaData();
+        inFile.close();
+    }
 
 public:
-    
+    RandomDirectFileRegistro() = default;
+
+    RandomDirectFileRegistro(std::string _dataFile){
+        dataFile = _dataFile;
+        indexFile = "index_" + dataFile;
+        initFiles();
+    }
+
+    void insertAll(std::vector<Registro> registros) {
+        std::cout << "\n** insertAll method **\n";
+        std::fstream outFile;
+        outFile.open(dataFile, std::ios::out |std::ios::app | std::ios::binary);
+        std::string key;
+        int row = 1;
+        for(auto registro : registros){
+            // Write object to Data file
+            outFile << registro;
+            // Update index File
+            updateIndexFile(registro, row);
+            row++;
+        }
+        outFile.close();
+    }
+
+    /**
+     * Load to Memory Random File
+     */
+    std::map<std::string, int> loadIndexFile(){
+        std::cout << "\n** Load Index File Method **\n";
+        std::map<std::string, int> indexMap;
+        std::ifstream inFile;
+        inFile.open(indexFile, std::ios::in | std::ios::binary);
+        // Check number of rows
+        inFile.seekg(0, std::ios::end);
+        int pos = inFile.tellg();
+        int sizeRecord = 5 + 4 + 1;         // size of record in Index File
+        int numRows =  (pos / (sizeRecord));
+        // Start Reading Index File
+        inFile.seekg(0, std::ios::beg);
+        char codigo[5];
+        int rowInDataFile;
+        std::string codigoKey;
+        for(int i = 1; i <= numRows; i++){
+            inFile.read(codigo, 5);
+            inFile.read(reinterpret_cast<char *>(&rowInDataFile), sizeof(rowInDataFile));
+            codigoKey = codigo;
+            indexMap.insert({codigoKey, rowInDataFile});
+            inFile.get();   // read endLine character
+        }
+        inFile.close();
+        return indexMap;
+    }
+
+    void search(std::map<std::string, int>& indexMap, std::string key){
+        std::cout << "\n*** Search method ***\n";
+        std::cout << "search key '" << key << "' in map (Memory)\n";
+        auto itrResult = indexMap.find(key);
+        if(itrResult != indexMap.end()){
+            std::cout << "Found row value: " << itrResult->second << "\n";
+            readRecord(itrResult->second);
+        }
+        else{
+            std::cout << "Key does not exist\n";
+        }
+    }
+
+    /**
+     * ScanFile: internal tool to verify all fields of DataFile and Index File
+     */
+    void scanFileDb(){
+        std::cout << "\n** Scan File DB **\n";
+        Registro record;
+        std::ifstream inFile;
+        inFile.open(dataFile, std::ios::in | std::ios::binary);
+        // Read Data File
+        std::cout << "\n** Data File DB **\n";
+        while(inFile >> record) {
+            record.showMetaData();
+        }
+        inFile.close();
+        // Read Aux File
+        inFile.open(indexFile, std::ios::in | std::ios::binary);
+        std::cout << "\n** Index File DB **\n";
+        inFile.seekg(0, std::ios::end);
+        int pos = inFile.tellg();
+        int sizeRecord = 5 + 4 + 1;         // size of record in Index File
+        int numRows =  (pos / (sizeRecord));
+        inFile.seekg(0, std::ios::beg);
+        char codigo[5];
+        int rowInDataFile;
+        for(int i = 1; i <= numRows; i++){
+            inFile.read(codigo, 5);
+            std::cout << codigo << ", ";
+            inFile.read(reinterpret_cast<char *>(&rowInDataFile), sizeof(rowInDataFile));
+            std::cout << rowInDataFile << "\n";
+            inFile.get();   // read endLine character
+        }
+        inFile.close();
+    }
 };
 
 #endif //LAB2_1_RANDOMDIRECTFILEREGISTRO_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
+
 #include "Registro.h"
 #include "SequentialFileRegistro.h"
+#include "RandomDirectFileRegistro.h"
 
 using namespace std;
 
@@ -25,56 +28,57 @@ int main() {
      * P1. a)
      * InsertAll(vector<Registro> registros)
      */
-    SequentialFileRegistro myFileRegistroDb("registros.txt");
-    // Insert all records to data File
-    myFileRegistroDb.insertAll(registros);
-    // Check metadata and verify (internal tool)
-    myFileRegistroDb.scanFileDb();
+//    SequentialFileRegistro myFileRegistroDb("registros.txt");
+//    // Insert all records to data File
+//    myFileRegistroDb.insertAll(registros);
+//    // Check metadata and verify (internal tool)
+//    myFileRegistroDb.scanFileDb();
 
     /**
      * P1. b)
      * binarySearch realiza un numero de Accesos a disco que es lg(n)
      */
-     int res = myFileRegistroDb.binarySearch("004");
-     std::cout << "binarySearch Result: record is located in row " << res << '\n';
+//     int res = myFileRegistroDb.binarySearch("004");
+//     std::cout << "binarySearch Result: record is located in row " << res << '\n';
      /**
       * P1. c)
       */
     // Insert
-    Registro reg3 = Registro("003", "CS", 5);
-    myFileRegistroDb.add(reg3);
-    myFileRegistroDb.scanFileDb();
+//    Registro reg3 = Registro("003", "CS", 5);
+//    myFileRegistroDb.add(reg3);
+//    myFileRegistroDb.scanFileDb();
+//
+//    // Insert
+//    Registro reg8 = Registro("008", "CS", 5);
+//    myFileRegistroDb.add(reg8);
+//    myFileRegistroDb.scanFileDb();
+//
+//    // Insert
+//    Registro reg9 = Registro("009", "CS", 5);
+//    myFileRegistroDb.add(reg9);
+//    myFileRegistroDb.scanFileDb();
+//
+//    // Insert
+//    Registro reg10 = Registro("010", "CS", 5);
+//    myFileRegistroDb.add(reg10);
+//    myFileRegistroDb.scanFileDb();
+//
+//    // Insert until reach 10 records
+//    Registro reg11 = Registro("011", "CS", 5);
+//    Registro reg12 = Registro("012", "CS", 5);
+//    Registro reg13 = Registro("013", "CS", 5);
+//    Registro reg14 = Registro("014", "CS", 5);
+//    Registro reg15 = Registro("015", "CS", 5);
+//    Registro reg16 = Registro("016", "CS", 5);
+//    myFileRegistroDb.add(reg11);
+//    myFileRegistroDb.add(reg12);
+//    myFileRegistroDb.add(reg13);
+//    myFileRegistroDb.add(reg14);
+//    myFileRegistroDb.add(reg15);
+//    myFileRegistroDb.scanFileDb();
+//    myFileRegistroDb.add(reg16);
+//    myFileRegistroDb.scanFileDb();
 
-    // Insert
-    Registro reg8 = Registro("008", "CS", 5);
-    myFileRegistroDb.add(reg8);
-    myFileRegistroDb.scanFileDb();
-
-    // Insert
-    Registro reg9 = Registro("009", "CS", 5);
-    myFileRegistroDb.add(reg9);
-    myFileRegistroDb.scanFileDb();
-
-    // Insert
-    Registro reg10 = Registro("010", "CS", 5);
-    myFileRegistroDb.add(reg10);
-    myFileRegistroDb.scanFileDb();
-
-    // Insert until reach 10 records
-    Registro reg11 = Registro("011", "CS", 5);
-    Registro reg12 = Registro("012", "CS", 5);
-    Registro reg13 = Registro("013", "CS", 5);
-    Registro reg14 = Registro("014", "CS", 5);
-    Registro reg15 = Registro("015", "CS", 5);
-    Registro reg16 = Registro("016", "CS", 5);
-    myFileRegistroDb.add(reg11);
-    myFileRegistroDb.add(reg12);
-    myFileRegistroDb.add(reg13);
-    myFileRegistroDb.add(reg14);
-    myFileRegistroDb.add(reg15);
-    myFileRegistroDb.scanFileDb();
-    myFileRegistroDb.add(reg16);
-    myFileRegistroDb.scanFileDb();
 
     /**
      * P2. Random File
@@ -84,5 +88,20 @@ int main() {
      * P2. a)
      */
 
+    std::cout << "\n****** P2. Random File *******\n";
+
+    // P2. a)
+    // Create Random File (disk)
+    RandomDirectFileRegistro myRandomFileDb("randomDB.dat");
+    myRandomFileDb.insertAll(registros);
+    myRandomFileDb.scanFileDb();
+
+    // Load to memory Index File (Create Map)
+    std::map<std::string, int> indexMap = myRandomFileDb.loadIndexFile();
+
+    // P2. b)
+    // Busqueda usando key-record in index Map
+    myRandomFileDb.search(indexMap, "006");     // search codigo '005'
+    myRandomFileDb.search(indexMap, "008");     // search codigo '005'
     return 0;
 }
